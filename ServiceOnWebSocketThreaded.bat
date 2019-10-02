de7a21818927dcae6d7cee8ed01f4260d666dc05
Asim Ishaq <asim709@gmail.com>
2019-10-02 15:21:52 +0500

    Added pthreads in Command Line
    A web request from Apache to PHP first loads all extension listed in PHP.ini and then serves the request. There appear to be some design level limitation with PHP and Pthreads such that in Fast CGI mode / Web request the pthreads module terminates with Fatal error. It appears that Pthreads in PHP 7.2 can only be used from CLI so i removed it from php.ini file and changed my command line to include pthreads while starting websocket server.

--- ServiceOnWebSocketThreaded.bat
+++ ServiceOnWebSocketThreaded.bat
@@ -1,4 +1,4 @@
 title WebSocket API Server @ 8088
 cls
-%cd%/../../server/php-7.2.23-Win32-VC15-x64/php.exe ServiceOnWebSocketThreaded.php
+%cd%/../../server/php-7.2.23-Win32-VC15-x64/php.exe -d extension=pthreads ServiceOnWebSocketThreaded.php
 pause
